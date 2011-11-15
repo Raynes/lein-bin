d9743495006d119d4a6c85591b9d9c86d3b131b5
Anthony Grimes <i@raynes.me>
2011-11-14 19:32:02 -0600

    Use :target-dir.

--- src/leiningen/bin.clj
+++ src/leiningen/bin.clj
@@ -12,13 +12,14 @@ Add :main to your project.clj to specify the namespace that contains your
 -main function."
   [project]
   (if (:main project)
-    (let [binfile (file (:name project))]
+    (let [opts (:jvm-opts project "")
+          target (file (:target-dir project))
+          binfile (file target (:name project))]
       (uberjar project)
       (println "Creating standalone executable:" (.getPath binfile))
       (with-open [bin (FileOutputStream. binfile)]
-        (let [opts (:jvm-opts project "")]
-          (.write bin (.getBytes (format ":;exec java %s -jar $0 \"$@\"\n" opts)))
-          (.write bin (.getBytes (format "@echo off\r\njava %s -jar %%1 \"%%~f0\" %%*\r\ngoto :eof\r\n" opts))))
-        (copy (file (str (get-default-uberjar-name project))) bin))
+        (.write bin (.getBytes (format ":;exec java %s -jar $0 \"$@\"\n" opts)))
+        (.write bin (.getBytes (format "@echo off\r\njava %s -jar %%1 \"%%~f0\" %%*\r\ngoto :eof\r\n" opts)))
+        (copy (file target (get-default-uberjar-name project)) bin))
       (.setExecutable binfile true))
     (println "Cannot create bin without :main namespace in project.clj")))
