8f6a4eea4fccf623e180d3584fa0210dea0190bf
Drew Raines <aaraines@gmail.com>
2013-01-17 14:54:49 -0600

    0.3.0-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-bin "0.2.2"
+(defproject lein-bin "0.3.0-SNAPSHOT"
   :description "A leiningen plugin for generating standalone console executables for your project."
     :url "https://github.com/Raynes/lein-bin"
   :license {:name "Eclipse Public License"
