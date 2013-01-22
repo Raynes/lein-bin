424fe8de43e6362590808611f8dce75748afec57
Anthony Grimes <i@raynes.me>
2013-01-22 16:27:31 -0600

    Release 0.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-bin "0.3.0-SNAPSHOT"
+(defproject lein-bin "0.3.0"
   :description "A leiningen plugin for generating standalone console executables for your project."
     :url "https://github.com/Raynes/lein-bin"
   :license {:name "Eclipse Public License"
