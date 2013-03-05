e617f705af3e22f53c30233d1562720ceb6bfece
Jason Whitlark <jwhitlark@bittorrent.com>
2013-03-05 15:26:08 -0800

    Added ability to set {:bin {:bin-path "/foo/bar"}} in project.clj or profiles.clj, so that after creating a binary, a second copy is made and set executable, in a directory that you have presumably added to your path.  This kinda emulates how the go command handles golang binaries, which can be handy.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-bin "0.3.0"
+(defproject lein-bin "0.3.1"
   :description "A leiningen plugin for generating standalone console executables for your project."
     :url "https://github.com/Raynes/lein-bin"
   :license {:name "Eclipse Public License"
