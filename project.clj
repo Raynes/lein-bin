098ea265f34aaa7c7007597de174a8f1b7b8a3ec
Anthony Grimes <i@raynes.me>
2013-03-05 20:00:08 -0800

    Fix indentation.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-bin "0.3.1"
   :description "A leiningen plugin for generating standalone console executables for your project."
-    :url "https://github.com/Raynes/lein-bin"
+  :url "https://github.com/Raynes/lein-bin"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
